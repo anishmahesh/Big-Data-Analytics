6c1d5a754d9f1a7708f9f3ffc44b2367a935a048
anishmahesh <anish.v.mahesh@gmail.com>
2017-04-26 17:06:58 -0400

    Working code for Area Classification

--- restaurants/src/AreaClassifier.java
+++ restaurants/src/AreaClassifier.java
@@ -1,9 +1,11 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.net.URI;
 
 /**
  * Created by anish on 4/25/17.
@@ -16,17 +18,19 @@ public class AreaClassifier {
         }
 
         Job job = new Job();
-        job.setJarByClass(DataCleaner.class);
-        job.setJobName("Data Cleaning");
+        job.setJarByClass(AreaClassifier.class);
+        job.setJobName("Area Classification");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(DataCleanerMapper.class);
-        job.setReducerClass(DataCleanerReducer.class);
+        job.setMapperClass(AreaClassifierMapper.class);
+        job.setReducerClass(AreaClassifierReducer.class);
         job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+
+        job.addCacheFile(new URI("rbda/area/input/AreaCentroid.csv"));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
--- restaurants/src/AreaClassifierMapper.java
+++ restaurants/src/AreaClassifierMapper.java
@@ -1,67 +1,106 @@
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.filecache.DistributedCache;
 
 /**
  * Created by anish on 4/25/17.
  */
 public class AreaClassifierMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 
+    class Area {
+        String name;
+        double latitude;
+        double longitude;
+
+        public Area(String name, double latitude, double longitude) {
+            this.name = name;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+    }
+
+    List<Area> areas = new ArrayList<>();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException
+    {
+        super.setup(context);
+
+        if (context.getCacheFiles() != null
+                && context.getCacheFiles().length > 0) {
+
+            BufferedReader br = new BufferedReader(new FileReader(new File("./AreaCentroid.csv")));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(",");
+                areas.add(new Area(words[0], Double.parseDouble(words[1]), Double.parseDouble(words[2])));
+            }
+
+        }
+        super.setup(context);
+    }
+
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
         Map<String, Integer> indexMap = new LinkedHashMap<>();
-        indexMap.put("name", 25);
-        indexMap.put("rating", 3);
-        indexMap.put("price", 19);
-        indexMap.put("parking", 6);
-        indexMap.put("alcohol", 10);
-        indexMap.put("payment_cashonly", 12);
-        indexMap.put("latitude", 15);
-        indexMap.put("longitude", 28);
-
-        if (key.get() == 0) return;
+        indexMap.put("latitude", 6);
+        indexMap.put("longitude", 7);
 
         String line = value.toString();
-        String[] vals = line.split("\t");
+        String[] vals = line.split(",");
 
-        if (vals[6] == null || vals[6].isEmpty()) {
-            vals[6] = "False";
+        double lat = Double.parseDouble(vals[indexMap.get("latitude")]);
+        double lon = Double.parseDouble(vals[indexMap.get("longitude")]);
+
+
+        double min = Double.MAX_VALUE;
+        String closestArea = areas.get(0).name;
+        for (Area a: areas) {
+            double dist = distance(lat, a.latitude, lon, a.longitude, 0, 0);
+            if (dist <= min) {
+                min = dist;
+                closestArea = a.name;
+            }
         }
 
-        if (vals[10] == null || vals[10].isEmpty()) {
-            vals[10] = "False";
-        }
-
-        if (vals[12] == null || vals[12].isEmpty()) {
-            vals[12] = "False";
-        }
-
-        if (vals[3] == null || vals[3].isEmpty()) {
-            vals[3] = "0";
-        }
-
-        if (vals[19] == null || vals[19].isEmpty()) {
-            vals[19] = "0";
-        }
-
-//        If either of latitude or longitude is null, omit the row.
-        if (vals[15] == null || vals[15].isEmpty() || vals[28] == null || vals[28].isEmpty()) {
-            return;
-        }
-
-        String csvRow = "";
-        String separator = "";
-        for (String col: indexMap.keySet()) {
-            csvRow += separator + vals[indexMap.get(col)];
-            separator = ",";
-        }
+        String csvRow = closestArea + "," + line;
 
         context.write(key, new Text(csvRow));
+    }
+
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * el2 End altitude in meters
+     * @returns Distance in Meters
+     */
+    public double distance(double lat1, double lat2, double lon1,
+                                  double lon2, double el1, double el2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
     }
 }
