bcc0b256abf51525f95db255a73a5850f56556eb
namanksrivastava <namanksrivastava@gmail.com>
2017-04-28 21:08:19 -0400

    script again : Naman

--- crime/src/AreaClassifier/run_script.sh
+++ crime/src/AreaClassifier/run_script.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 
 javac -classpath `yarn classpath`:. -d . AreaClassifier.java
 jar cvf areaClassifier.jar *.class
-rm -rf  project/output/Zipcode1
+hdfs dfs -rm -r -f  project/output/Zipcode1
 hadoop jar areaClassifier.jar AreaClassifier project/input/NEW_FINAL_NYPD_Complaint_Data_Historic.csv project/output/Zipcode1
