3e09fc04843eb2796f298f397177d9a489b0fad5
namanksrivastava <namanksrivastava@gmail.com>
2017-04-30 20:10:02 -0400

    test final: Naman

--- crime/src/AreaClassifier/run_script.sh
+++ crime/src/AreaClassifier/run_script.sh
@@ -2,5 +2,5 @@
 
 javac -classpath `yarn classpath`:. -d . AreaClassifier.java
 jar cvf areaClassifier.jar *.class
-hdfs dfs -rm -r -f  project/output/Zipcode1
-hadoop jar areaClassifier.jar AreaClassifier project/input/FINAL_REQUIRED.csv project/output/Zipcode1
+hdfs dfs -rm -r -f  project/output/Zipcode3
+hadoop jar areaClassifier.jar AreaClassifier project/input/FINAL_REQUIRED.csv project/output/Zipcode3
