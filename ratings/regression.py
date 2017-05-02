eabb3d0175009c278d9e65b35882f069f1d56326
anishmahesh <anish.v.mahesh@gmail.com>
2017-05-02 04:03:46 -0400

    Added column selection script

--- ratings/regression.py
+++ ratings/regression.py
@@ -8,8 +8,8 @@ spark = SparkSession\
 
 spark.sparkContext.setLogLevel('ERROR')
 
-training = spark.read.format("libsvm").load('spark_ex_data/input/rbda_data.txt')
-train, test = training.randomSplit([0.9, 0.1])
+training = spark.read.format("libsvm").load('spark_ex_data/input/spark_reg_input.txt')
+train, test = training.randomSplit([0.98, 0.02])
 
 lr = LinearRegression(maxIter=10, regParam=0.01, elasticNetParam=0.0)
 lrModel = lr.fit(train)
@@ -24,6 +24,6 @@ rmse = prediction.rdd.map(lambda r: ((r[0] - r[2])**2)).mean()**.5
 
 print("Test RMSE: %f" % rmse)
 
-# pr = prediction.rdd.map(lambda r: (r[0], r[2])).collect()
-# for p in pr:
-#     print(p[0],p[2])
+pr = prediction.rdd.map(lambda r: (r[0], r[2])).collect()
+for p in pr:
+    print(p[0],p[2])
