a2b77510ab9db2f067497d2b8c77d2cc600944ed
anishmahesh <anish.v.mahesh@gmail.com>
2017-05-02 02:46:48 -0400

    Pyspark regression code

---
+++ ratings/regression.py
@@ -0,0 +1,21 @@
+from pyspark.ml.regression import LinearRegression
+
+training = spark.read.format("libsvm").load('spark_ex_data/input/rbda_data.txt')
+train, test = training.randomSplit([0.9, 0.1])
+
+lr = LinearRegression(maxIter=10, regParam=0.01, elasticNetParam=0.0)
+lrModel = lr.fit(train)
+trainingSummary = lrModel.summary
+print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
+print("Coefficients: %s" % str(lrModel.coefficients))
+
+prediction = lrModel.transform(test)
+prediction.describe().show()
+
+rmse = prediction.rdd.map(lambda r: ((r[0] - r[2])**2)).mean()**.5
+
+print("Test RMSE: %f" % rmse)
+
+# pr = prediction.rdd.map(lambda r: (r[0], r[2])).collect()
+# for p in pr:
+#     print(p[0],p[2])
