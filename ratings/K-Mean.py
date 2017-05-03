1e5b74ce3b1db2456b5285c18c91baab0c45b3ec
namanksrivastava <namanksrivastava@gmail.com>
2017-05-02 23:49:22 -0400

    Added K-mean

---
+++ ratings/K-Mean.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from pyspark.ml.clustering import KMeans
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+    .builder\
+    .appName("KMeansExample")\
+    .getOrCreate()
+
+
+spark.sparkContext.setLogLevel('ERROR')
+
+dataset = spark.read.format("libsvm").load("data/mllib/sample_kmeans_data.txt")
+
+kmeans = KMeans().setK(5).setSeed(1)
+model = kmeans.fit(dataset)
+
+wssse = model.computeCost(dataset)
+print("Within Set Sum of Squared Errors = " + str(wssse))
+
+centers = model.clusterCenters()
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
+
+spark.stop()
