e3b58a2149a19236cf7c2511cc176776758edfc7
namanksrivastava <namanksrivastava@gmail.com>
2017-05-03 02:34:39 -0400

    revert K-Mean:Naman

--- ratings/K-Mean.py
+++ ratings/K-Mean.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-from pyspark.mllib.clustering import KMeans, KMeansModel
+from pyspark.ml.clustering import KMeans
 from pyspark.sql import SparkSession
-import numpy as np
-from math import sqrt
 
 spark = SparkSession\
     .builder\
@@ -12,35 +10,15 @@ spark = SparkSession\
 
 spark.sparkContext.setLogLevel('ERROR')
 
-data = spark.sparkContext.textFile("project/input/lat_long_list.txt")
+dataset = spark.read.format("libsvm").load("project/input/list1.txt")
 
-parsedData = data.map(lambda line: np.array([float(x) for x in line.split(',')]))
+kmeans = KMeans().setK(5).setSeed(1)
+model = kmeans.fit(dataset)
 
+wssse = model.computeCost(dataset)
+print("Within Set Sum of Squared Errors = " + str(wssse))
 
-clusters = KMeans.train(parsedData, 40, maxIterations=5, initializationMode="random")
-
-def error(point):
-    center = clusters.centers[clusters.predict(point)]
-    return sqrt(sum([x**2 for x in (point - center)]))
-
-WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
-print("Within Set Sum of Squared Error = " + str(WSSSE))
-
-center_dict = {}
-def createDict(point):
-    center = clusters.centers[clusters.predict(point)]
-    if not center in center_dict:
-        center_dict[center] = []
-    center_dict[center].append(point)
-
-parsedData.map(lambda point: createDict(point))
-
-print(center_dict)
-
-count = 0
-for k in sorted(center_dict, key=lambda k: len(center_dict[k]), reverse=True):
-        if not count == 5:
-            print(k +  '\t' + center_dict[k])
-        else:
-            break
-        count += 1
+centers = model.clusterCenters()
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
