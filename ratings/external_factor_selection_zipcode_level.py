85d11213bcabb84fd7b13b7c65ff50a252d170de
anishmahesh <anish.v.mahesh@gmail.com>
2017-05-03 21:01:55 -0400

    Feature selection

--- ratings/external_factor_selection_zipcode_level.py
+++ ratings/external_factor_selection_zipcode_level.py
@@ -16,7 +16,7 @@ spark.sparkContext.setLogLevel('ERROR')
 model_to_use = 'linear'
 # model_to_use = 'random_forest'
 
-label_col = 'grade_C_pct'
+label_cols = ['grade_A_pct', 'grade_B_pct', 'grade_C_pct']
 cols_to_keep = ['crime_sev1','crime_sev2','crime_attempted','crime_completed','violation','misdemeanor','felony','311_sev1','311_sev2','311_sev3','311_sev4','avg_cost','avg_tip','passenger_count']
 pre_label_cols = ['grade_A_count','grade_B_count','grade_C_count']
 
@@ -31,29 +31,46 @@ df = df.withColumn('grade_C_pct', df.grade_C_count / (df.grade_A_count + df.grad
 
 assembler = VectorAssembler(inputCols=cols_to_keep, outputCol='features')
 
-training = assembler.transform(df).select(col(label_col).alias('label'), 'features')
+transformed = assembler.transform(df)
+
+for label_col in label_cols:
+    training = transformed.select(col(label_col).alias('label'), 'features')
+
+    print('==========='+label_col+'=============\n')
+
+    # Running linear regression with elastic net
+    print('\n-----Elastic Net-------\n')
+
+    lr = LinearRegression(maxIter=10, regParam=0.01, elasticNetParam=0.5)
+    lrModel = lr.fit(training)
+    trainingSummary = lrModel.summary
+    print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
+    coeffs = lrModel.coefficients.toArray()
+
+    features_selected = [(z[0], z[1]) for z in zip(cols_to_keep, coeffs) if not z[1] == 0]
+    features_selected = sorted(features_selected, key=lambda x: abs(x[1]))[::-1]
+
+    print('Features selected:\n')
+    for (feature, coeff) in features_selected:
+        print(feature, coeff)
 
 
-# Running linear regression with elastic net
-print('\n-----Elastic Net-------\n')
+    # Running random forest
+    print('\n-----Random Forest-------\n')
 
-lr = LinearRegression(maxIter=10, regParam=0.01, elasticNetParam=0.5)
-lrModel = lr.fit(training)
-trainingSummary = lrModel.summary
-print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
-print("Coefficients: %s" % str(lrModel.coefficients))
+    featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(training)
+    rf = RandomForestRegressor(featuresCol="indexedFeatures")
 
+    # Chain indexer and forest in a Pipeline
+    pipeline = Pipeline(stages=[featureIndexer, rf])
 
-# Running random forest
-print('\n-----Random Forest-------\n')
+    # Train model.  This also runs the indexer.
+    model = pipeline.fit(training)
+    rfModel = model.stages[1]
+    rf_coeffs = rfModel.featureImportances.toArray()
+    rf_features_selected = [(z[0], z[1]) for z in zip(cols_to_keep, rf_coeffs)]
+    rf_features_selected = sorted(rf_features_selected, key=lambda x: x[1])[::-1]
 
-featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(training)
-rf = RandomForestRegressor(featuresCol="indexedFeatures")
-
-# Chain indexer and forest in a Pipeline
-pipeline = Pipeline(stages=[featureIndexer, rf])
-
-# Train model.  This also runs the indexer.
-model = pipeline.fit(training)
-rfModel = model.stages[1]
-print(rfModel.featureImportances)
+    print('Features selected:\n')
+    for f in rf_features_selected[:len(features_selected)]:
+        print(f)
